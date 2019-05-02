d2439cd9d9f3523bc77966f1e0cff0bb322057d7
Anubha Vijay <anubha.vijay@hivehome.com>
2019-05-02 15:05:07 +0100

    Some more tests added

--- ValueCrossingEvents/tests.py
+++ ValueCrossingEvents/tests.py
@@ -16,21 +16,37 @@ class TestValueCrossingEvent(unittest.TestCase):
         print (json.loads(resp.text))
         return (json.loads(resp.text)["token"])
 
-    def test_get_value_crossing_events(self):
+    def test_api_accessible(self):
         token=self.get_user_authentication_token()
         resp = requests.get('http://localhost:8000/api/',headers={ 'Authorization': 'Token {}'.format(token)} )
-        print (resp.status_code, resp.text)
         self.assertEqual(resp.status_code, 200)
+
+    def test_incorrect_api_url(self):
+        token=self.get_user_authentication_token()
+        resp = requests.get('http://localhost:8000/aipi/',headers={ 'Authorization': 'Token {}'.format(token)} )
+        print(resp.status_code, resp.text)
+        self.assertEqual(resp.status_code, 404)
+
+    def test_authenticate(self):
+        resp = requests.get('http://localhost:8000/api/')
+        print(resp.status_code, resp.text)
+        self.assertEqual(resp.status_code, 401)
+        self.assertEqual(json.loads(resp.text)["detail"], "Authentication credentials were not provided.")
 
     def test_post_value_crossing_events(self):
         token = self.get_user_authentication_token()
         headers = { 'Authorization': 'Token {}'.format(token),
                     'Content-Type': 'application/json; charset=utf-8'
                   }
-        data = {"signal":[1,2,3,3,3,7,8,5,7,3,7,3], "value":3}
+        data = {"signal":[1,2,3,3,3,7,8,5,4,3,7,2], "value":3}
         resp = requests.post('http://localhost:8000/api/',data=json.dumps(data), headers=headers)
         print(resp.status_code, resp.text)
 
+    def test_get_tornado_app(self):
+        credentials = {'apiuser1': 'password'}
+        resp = requests.get('http://localhost:8888/?value=3&signal=1,2,3,3,3,7,8,5,4,3,7,2')
+        print(resp.status_code, resp.text)
+        #self.assertEqual(resp.status_code, 200)
 
 
 
--- tornadoapp.py
+++ tornadoapp.py
@@ -4,17 +4,17 @@ from tornado.web import RequestHandler, Application
 from tornado_http_auth import BasicAuthMixin
 from crossingevents.ValueCrossingEvents.crossingEvents import CrossingEvents
 
-credentials = {'apiuser1': 'password'}
-class MainHandler(BasicAuthMixin, RequestHandler):
-    def prepare(self):
-        self.get_authenticated_user(check_credentials_func=credentials.get, realm='Protected')
+#credentials = {'apiuser1': 'password'}
+class MainHandler(RequestHandler):
+   # def prepare(self):
+    #    self.get_authenticated_user(check_credentials_func=credentials.get, realm='Protected')
 
     def get(self):
         value = int(self.get_argument('value'))
         signal = self.get_argument('signal')
         signal = [int(sig) for sig in signal.split(',')]
         print (signal)
-        sum = CrossingEvents.get_number_of_value_crossings(signal, value)
+        sum = CrossingEvents(signal, value).get_number_of_value_crossings()
         self.write({'sum_of_crossing_events': sum})
 
 
